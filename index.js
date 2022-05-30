f1921d653f0e0bf319707b1f5b0a6dc425bf491f
Justin McCraw <jmccraw@ufl.edu>
2022-05-29 21:42:01 -0400

    initial setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+var questionsArr = [
+    {
+        question: 'JavaScript is the best language',
+        answer: true
+      }
+]
+
+function runQuiz {
+    alert('Your score is')
+}
