15cb0cbce08c0f29375b97f71a6dfa78d809348e
Justin McCraw <jmccraw@ufl.edu>
2022-06-02 14:55:25 -0400

    setup tests successful

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Your code here
 var questionsArr = [
     {
-        question: 'JavaScript is the best language',
+        question: 'NFL players were nott required to wear helmets until 1943',
         answer: true
+    },
+
+    {  question: 'A 30 second commercial in the 2021 Super Bowl cost over $5 million',
+        answer: true
+    },
+
+    {  question:'The Dallas Cowboys is worth over $9B and is one of the most valuable franchises in all of sports',
+        answer: false
+    },
+
+    {   question:'Eli and Peyton Manning are the only brothers to both win the Super Bowl MVP',
+        answer: true
+    },
+
+    {   question: 'The top 10 teams from each conference get into the playoffs',
+        answer: false
     }
 
       
