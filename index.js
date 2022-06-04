ff1b4469ae4313c0d022f5aa33dfda2060b673a2
Justin McCraw <jmccraw@ufl.edu>
2022-06-04 17:25:00 -0400

    runQuiz function added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 var questionsArr = [
     {
-        question: 'NFL players were nott required to wear helmets until 1943',
+        question: 'NFL players were not required to wear helmets until 1943',
         answer: true
     },
 
@@ -9,21 +9,35 @@ var questionsArr = [
         answer: true
     },
 
-    {  question:'The Dallas Cowboys is worth over $9B and is one of the most valuable franchises in all of sports',
-        answer: false
+    {  question:'The Dallas Cowboys is worth over $6B and is one of the most valuable franchises in all of sports',
+        answer: true
     },
 
     {   question:'Eli and Peyton Manning are the only brothers to both win the Super Bowl MVP',
         answer: true
     },
 
-    {   question: 'The top 10 teams from each conference get into the playoffs',
-        answer: false
+    {   question: 'The top 7 teams from each conference get into the playoffs',
+        answer: true
     }
 
-      
 ]
 
+
 function runQuiz () {
-    alert('Your score is')
+    for (var i = 0; i < questionsArr.length; i++) {
+        var questions = questionsArr [i]
+        var askQuestion = confirm(questions.question)
+        
+        var userScore = 0 
+
+        if (askQuestion && questions.answer) {
+            userScore + 20
+            console.log(userScore)
+        }
+    }
+    if (userScore <= 20) {
+        alert('Your score is ' + userScore)
+    }
+
 }
